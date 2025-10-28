fe9c57efa832fc81bd323845eb113ddd92034721
Darin Tsankov <darints19@gmail.com>
2025-10-28 09:10:41 +0200

    Da se kefish

--- GitExercise-master/GitExercise/OptionsManager.cs
+++ GitExercise-master/GitExercise/OptionsManager.cs
@@ -4,25 +4,40 @@ namespace GitExercise
 {
     public static class OptionsManager
     {
-        public static string[] OptionsList = {
-            "a - Add",
-            "s - Subtract",
-            "m - Multiply"
-        };
+        public static string[] OptionsList =
+        {
+        "a - Add",
+        "s - Subtract",
+        "m - Multiply",
+        "dr - Divide Remainder",
+        "ex - Exit"
+    };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
 
-        public static void Multiply(double a, double b)
-        {
-            Console.WriteLine($"{a} x {b} = {a * b}");
-        }
-
         public static void Subtract(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
         }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+
+        public static void DivideRemainder(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed!");
+                return;
+            }
+
+            Console.WriteLine($"{a} % {b} = {a % b}");
+        }
     }
+
 }
