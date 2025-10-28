2a39d39d8b0a5455526a8adbf08beda0d13d603c
dimitrovaviki07@gmail.com <youremail@mail.com>
2025-10-28 09:38:40 +0200

    Save current work

---
+++ GitExercise/CalculatorApp-Developer-B/GitExercise-master/GitExercise/OptionsManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList =
+        {
+        "a - Add",
+        "s - Subtract",
+        "m - Multiply",
+        "dr - Divide Remainder",
+        "ex - Exit"
+    };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+
+        public static void DivideRemainder(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed!");
+                return;
+            }
+
+            Console.WriteLine($"{a} % {b} = {a % b}");
+        }
+    }
+
+}
